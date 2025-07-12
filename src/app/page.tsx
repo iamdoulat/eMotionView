0f0ef7b757727f3eb71d3428158415515a6d7732
Smart Ideas <smartideasbd24@gmail.com>
2025-07-12 04:54:53 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <section class="co

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -162,7 +162,7 @@ export default async function HomePage() {
                                 <Link href={permalink}>See all</Link>
                             </Button>
                         </div>
-                        <div className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:gap-x-6">
+                        <div className="grid grid-cols-2 gap-x-4 gap-y-8 md:grid-cols-4 lg:grid-cols-6 xl:gap-x-6">
                             {products.map((product) => (
                                 <ProductCard key={product.id} product={product} />
                             ))}
