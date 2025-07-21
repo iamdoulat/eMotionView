735aaa5ff8307e4eeebc53dcb66a7587496181c4
Smart Ideas <smartideasbd24@gmail.com>
2025-07-21 04:07:15 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -1,11 +1,15 @@
 import * as React from "react"
+import { useHasMounted } from "./use-has-mounted"
 
 const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
+  const hasMounted = useHasMounted();
   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
 
   React.useEffect(() => {
+    if (!hasMounted) return;
+
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
     const onChange = () => {
       setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
@@ -13,7 +17,9 @@ export function useIsMobile() {
     mql.addEventListener("change", onChange)
     setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
     return () => mql.removeEventListener("change", onChange)
-  }, [])
+  }, [hasMounted])
+
+  if (!hasMounted) return undefined;
 
   return !!isMobile
 }
